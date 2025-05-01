33bdcbf59bde63e50cd921ca67cf4d83546ed5e4
Nathaandev <nathaanprogramador@hotmail.com>
2025-04-30 21:36:16 -0300

    Product controller HTTP

--- src/main/java/com/example/curso/controllers/ProductController.java
+++ src/main/java/com/example/curso/controllers/ProductController.java
@@ -1,7 +1,15 @@
 package com.example.curso.controllers;
 
+import com.example.curso.dtos.ProductRecordDTO;
+import com.example.curso.models.ProductModel;
 import com.example.curso.repository.ProductRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +18,14 @@ public class ProductController {
     @Autowired
     ProductRepository productRepository;
 
+    //Indicates to SpringBoot that this method will receive a POST request
+    @PostMapping("/products")
+    //Indicates that this method will be executed when the endpoint /products is called with the method HTTP Post
+
+    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDTO productRecordDTO){
+        var productModel = new ProductModel();
+        BeanUtils.copyProperties(productRecordDTO, productModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+    }
 
 }
