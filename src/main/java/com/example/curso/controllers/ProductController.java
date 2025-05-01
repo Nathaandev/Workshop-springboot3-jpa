75ea739c88c2d41d26e0527bcc45f2b00f5a7ea4
Nathaandev <nathaanprogramador@hotmail.com>
2025-04-30 22:18:54 -0300

    Post Method

--- src/main/java/com/example/curso/controllers/ProductController.java
+++ src/main/java/com/example/curso/controllers/ProductController.java
@@ -23,9 +23,14 @@ public class ProductController {
     //Indicates that this method will be executed when the endpoint /products is called with the method HTTP Post
 
     public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDTO productRecordDTO){
+        //The ResponseEntity Contains the HTTP response code and the body of the response
+        //The RequestBody annotation will take the JSON and transform it into a ProductRecordDTO object (Name and Price)
+        //The Valid annotation activates the @NotNull and @NotBlank annotations on the ProductRecordDTO class
         var productModel = new ProductModel();
-        BeanUtils.copyProperties(productRecordDTO, productModel);
+        BeanUtils.copyProperties(productRecordDTO, productModel); //Will copy the values from the ProductRecordDTO to the ProductModel (Name and Price)
+
         return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+        //Returns a HTTP response code 201 (Created) and the ProductModel object saved in the database
     }
 
 }
